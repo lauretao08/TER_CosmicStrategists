5555797cc1dfe44ce3e8d869adf8736720b5cb71
lauretao08 <31769320+lauretao08@users.noreply.github.com>
2020-02-20 01:43:59 +0100

    basic cardplayer

---
+++ CosmicStrategists/Assets/Scripts/CardPlayer/CardPlayer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardPlayer : MonoBehaviour
+{
+
+    private List<Card> deck;
+    private List<Card> draw_pile;
+    private List<Card> hand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        deck = new List<Card>();
+        
+        hand = new List<Card>();
+        deck.Add(new Card());
+        deck.Add(new Card());
+        deck.Add(new Card());
+
+        draw_pile = new List<Card>(deck);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Draw(int nb)
+    {
+        Card tmp;
+        for (int i = 0; i < nb; i++)
+        {
+            if (draw_pile.Count > 1)
+            {
+                tmp = draw_pile[0];
+                draw_pile.RemoveAt(0);
+                hand.Add(tmp);
+                Debug.Log("Card drawn : " + tmp.card_name);
+            }
+            else
+            {
+                Debug.Log("Empty draw pile, cannot draw card");
+            }
+            
+        }
+    }
+}
---
+++ CosmicStrategists/Assets/Scripts/UI/TestGameUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestGameUI : MonoBehaviour
+{
+    public Button drawOneCard;
+    public Button discardAllCards;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        drawOneCard.onClick.AddListener(delegate () { DrawOne(); });
+        discardAllCards.onClick.AddListener(delegate () { DiscardAll(); });
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DrawOne()
+    {
+        Debug.Log("Draw one card");
+    }
+
+    void DiscardAll()
+    {
+        Debug.Log("Discard all cards");
+    }
+}

9d122aabaa12f34dcb0997118e9588588f3ec8a0
lauretao08 <31769320+lauretao08@users.noreply.github.com>
2020-03-09 16:58:26 +0100

    finished very simple feedback on all cards
    might have to make it card-specific for ease of understanding

--- CosmicStrategists/Assets/Scripts/UI/CardFeedbackUI.cs
+++ CosmicStrategists/Assets/Scripts/UI/CardFeedbackUI.cs
@@ -8,6 +8,12 @@ public class CardFeedbackUI : MonoBehaviour
     public Text CardPlayer1;
     public Text CardPlayer2;
 
+    private bool c_displayed_1 = false;
+    private bool c_displayed_2 = false;
+
+    private float c_timer_1 = 0;
+    private float c_timer_2 = 0;
+
     public float timeDisplayed = 2.0f;
 
 
@@ -20,26 +26,49 @@ public class CardFeedbackUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //erase text if timer elapsed
+        if (c_displayed_1)
+        {
+            c_timer_1 += Time.deltaTime;
+            if (c_timer_1 > timeDisplayed)
+            {
+                Eraseplayer1();
+                c_timer_1 = 0.0f;
+            }
+        }
+
+        if (c_displayed_2)
+        {
+            c_timer_2 += Time.deltaTime;
+            if (c_timer_2 > timeDisplayed)
+            {
+                Eraseplayer2();
+                c_timer_2 = 0.0f;
+            }
+        }
     }
 
     public void WritePlayer1(string text)
     {
         CardPlayer1.text = text;
+        c_displayed_1 = true;
     }
 
     public void WritePlayer2(string text)
     {
         CardPlayer2.text = text;
+        c_displayed_2 = true;
     }
 
     void Eraseplayer1()
     {
         CardPlayer1.text = "";
+        c_displayed_1 = false;
     }
 
     void Eraseplayer2()
     {
         CardPlayer2.text = "";
+        c_displayed_2 = false;
     }
 }
